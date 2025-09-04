0a662064849737b88de8ccfa80c8165b9f7ae16e
Keerthika Chokkalingam <142724355+KeerthikaChokkalingam@users.noreply.github.com>
2025-09-04 23:11:16 +0530

    commit with project creation initial setup

---
+++ VideoDownloader/VideoDownloaderTests/VideoDownloaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  VideoDownloaderTests.swift
+//  VideoDownloaderTests
+//
+//  Created by Keerthika on 04/09/25.
+//
+
+import Testing
+@testable import VideoDownloader
+
+struct VideoDownloaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
